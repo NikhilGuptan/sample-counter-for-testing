5688b238c75c9a601da68ed5feb03c68026f0ed5
NikhilGuptan <ng3152340@gmail.com>
2024-08-23 11:25:51 +0530

    added the code

--- src/js/index.js
+++ src/js/index.js
@@ -15,7 +15,7 @@ function SimpleCounter(props) {
 			<div className="calendar">
 				<i className="far fa-clock" />
 			</div>
-			<div className="four">{props.digitFour % 10}</div>
+			<div className="four">{props.digitFour % 10}</div>  
 			<div className="three">{props.digitThree % 10}</div>
 			<div className="two">{props.digitTwo % 10}</div>
 			<div className="one">{props.digitOne % 10}</div>
